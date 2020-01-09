1bdae9fb856f6da6135bcb46416328b20c3d1b39
Smirnova <smirnova270694@gmail.com>
2020-01-09 02:43:15 +0200

    remove front-end folders

--- public/js/question-type.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    let questionType = $('#questionType');
-    let questionTypeValue;
-    let typeValue = $('#typeValue');
-    questionType.change(function () {
-        questionTypeValue = questionType.val();
-        typeValue.val(questionTypeValue);
-    });
-});
